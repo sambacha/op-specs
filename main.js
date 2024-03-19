c7e2c1c825d2df9a63b058135037fa85cd6a622e
sam bacha <sam@manifoldfinance.com>
2024-03-19 16:29:25 -0700

    update

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fastest_levenshtein_1 = require("fastest-levenshtein");
-var AnchorJS = require("anchor-js");
-var anchors = new AnchorJS();
-document.addEventListener('turbo:load', function () {
-    anchors.add();
-    var div = document.getElementById('four-oh-four-suggestion');
-    if (div) {
-        var xhr_1 = new XMLHttpRequest();
-        xhr_1.onload = function () {
-            if (xhr_1.status === 200) {
-                var xml = xhr_1.responseXML;
-                var urls = Array.from(xml.querySelectorAll('urlset > url > loc')).map(function (el) { return el.textContent; });
-                var url = new URL((0, fastest_levenshtein_1.closest)(window.location.href, urls));
-                div.innerHTML = "<a href=\"".concat(url.href, "\">").concat(url.pathname, "</a>");
-            }
-            else {
-                div.innerHTML = '<a href="/">/</a>';
-            }
-        };
-        xhr_1.open('GET', "".concat(window.location.protocol, "//").concat(window.location.host, "/sitemap.xml"));
-        xhr_1.send();
-    }
-});
+var div = document.getElementById('four-oh-four-suggestion');
+if (div) {
+    var xhr_1 = new XMLHttpRequest();
+    xhr_1.onload = function () {
+        if (xhr_1.status === 200) {
+            var xml = xhr_1.responseXML;
+            // @ts-ignore
+            var urls = Array.from(xml.querySelectorAll('urlset > url > loc')).map(function (el) { return el.textContent; });
+            // @ts-ignore
+            var url = new URL((0, fastest_levenshtein_1.closest)(window.location.href, urls));
+            div.innerHTML = "<a href=\"".concat(url.href, "\">").concat(url.pathname, "</a>");
+        }
+        else {
+            div.innerHTML = '<a href="/">/</a>';
+        }
+    };
+    xhr_1.open('GET', "".concat(window.location.protocol, "//").concat(window.location.host, "/sitemap.xml"));
+    xhr_1.send();
+}
--- main.ts
+++ main.ts
@@ -1,29 +1,22 @@
 import { closest } from 'fastest-levenshtein';
 
-import * as AnchorJS from 'anchor-js';
-import * as Turbo from '@hotwired/turbo';
+const div = document.getElementById('four-oh-four-suggestion');
+if (div) {
+  const xhr = new XMLHttpRequest();
 
-const anchors = new AnchorJS();
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const xml = xhr.responseXML;
+         // @ts-ignore
+      const urls = Array.from(xml.querySelectorAll('urlset > url > loc')).map((el) => el.textContent);
+      // @ts-ignore
+      const url = new URL(closest(window.location.href, urls));
+      div.innerHTML = `<a href="${url.href}">${url.pathname}</a>`;
+    } else {
+      div.innerHTML = '<a href="/">/</a>';
+    }
+  };
 
-document.addEventListener('turbo:load', () => {
-  anchors.add();
-
-  const div = document.getElementById('four-oh-four-suggestion');
-  if (div) {
-    const xhr = new XMLHttpRequest();
-
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        const xml = xhr.responseXML;
-        const urls = Array.from(xml.querySelectorAll('urlset > url > loc')).map((el) => el.textContent);
-        const url = new URL(closest(window.location.href, urls));
-        div.innerHTML = `<a href="${url.href}">${url.pathname}</a>`;
-      } else {
-        div.innerHTML = '<a href="/">/</a>';
-      }
-    };
-
-    xhr.open('GET', `${window.location.protocol}//${window.location.host}/sitemap.xml`);
-    xhr.send();
-  }
-});
+  xhr.open('GET', `${window.location.protocol}//${window.location.host}/sitemap.xml`);
+  xhr.send();
+}
